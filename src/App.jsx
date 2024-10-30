99992ccdccc687acdfa8ba458d108a0230a2839a
Anikcse19 <anikdebcse@gmail.com>
2024-10-31 01:31:10 +0600

    settle list and unsettle list

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 // import DashBoardHome from "./pages/Dashboard/DashBoardHome";
-import UnsettleBet from "./pages/Dashboard/Settlement/UnsettleBet";
+// import UnsettleBet from "./pages/Dashboard/Settlement/UnsettleBet";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
     <>
-      <UnsettleBet />
+      <LoginPage />
     </>
   );
 }

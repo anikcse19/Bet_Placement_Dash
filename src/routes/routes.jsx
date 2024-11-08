6e2017f50849b1d33da0b33437f062e1ff2e5306
Anikcse19 <anikdebcse@gmail.com>
2024-11-08 20:30:54 +0600

    add vercel json file

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,5 +1,4 @@
 import { createBrowserRouter } from "react-router-dom";
-import App from "../App";
 import LoginPage from "../pages/LoginPage";
 import SetteBet from "../pages/Dashboard/Settlement/SetteBet";
 import UnsettleBet from "../pages/Dashboard/Settlement/UnsettleBet";
@@ -14,7 +13,11 @@ import UpdateUser from "../pages/Dashboard/UpdateUser";
 export const routes = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: (
+      <PrivateRoute>
+        <UnsettleBet />
+      </PrivateRoute>
+    ),
   },
   {
     path: "/login",
